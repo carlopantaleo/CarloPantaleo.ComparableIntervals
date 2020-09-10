8761a9648e3e360d746ee2c4b6c3689bfdc45853
Carlo Pantaleo <carlo.pantaleo@icloud.com>
2020-09-10 16:37:41 +0200

    Create Intervals utility class and its first method Simplify

---
+++ CarloPantaleo.ComparableIntervals.Tests/IntervalsTest.cs
@@ -0,0 +1,89 @@
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace CarloPantaleo.ComparableIntervals.Tests {
+    public class IntervalsTest {
+        [Theory]
+        [MemberData(nameof(SimplifyData))]
+        public void Simplify(List<Interval<int>> input, List<Interval<int>> expectedOutput) {
+            var output = Intervals.Simplify(input);
+            // Comparison is done on the string representation of intervals in order to handle infinity cases which are
+            // not comparable.
+            Assert.Equal(expectedOutput.Select(i => i.ToString()), output.Select(i => i.ToString()));
+        }
+
+        public static IEnumerable<object[]> SimplifyData =>
+            new List<object[]> {
+                new object[] {
+                    new List<Interval<int>> {
+                        Interval<int>.Closed(1, 3), Interval<int>.Open(4, 7), Interval<int>.Closed(5, 8)
+                    },
+                    new List<Interval<int>> {
+                        Interval<int>.Closed(1, 3), Interval<int>.OpenClosed(4, 8)
+                    }
+                },
+                new object[] {
+                    new List<Interval<int>> {
+                        Interval<int>.Open(4, 7), Interval<int>.Closed(5, 8), Interval<int>.Closed(1, 3)
+                    },
+                    new List<Interval<int>> {
+                        Interval<int>.Closed(1, 3), Interval<int>.OpenClosed(4, 8)
+                    }
+                },
+                new object[] {
+                    new List<Interval<int>> {
+                        Interval<int>.OpenClosed(0, 3), Interval<int>.Open(1, 3), Interval<int>.Open(4, 7),
+                        Interval<int>.Closed(5, 8)
+                    },
+                    new List<Interval<int>> {
+                        Interval<int>.OpenClosed(0, 3), Interval<int>.OpenClosed(4, 8)
+                    }
+                },
+                new object[] {
+                    new List<Interval<int>> {
+                        Interval<int>.OpenClosed(0, 3), Interval<int>.Open(1, 3), Interval<int>.Open(2, 4), 
+                        Interval<int>.Open(5, 7), Interval<int>.Closed(5, 9)
+                    },
+                    new List<Interval<int>> {
+                        Interval<int>.Open(0, 4), Interval<int>.Closed(5, 9)
+                    }
+                },
+                new object[] {
+                    new List<Interval<int>> {
+                        Interval<int>.Closed(5, 9), Interval<int>.OpenClosed(0, 3), Interval<int>.Open(1, 3), 
+                        Interval<int>.Open(2, 4), Interval<int>.Open(5, 7)
+                    },
+                    new List<Interval<int>> {
+                        Interval<int>.Open(0, 4), Interval<int>.Closed(5, 9)
+                    }
+                },
+                new object[] {
+                    new List<Interval<int>> {
+                        Interval<int>.Closed(5, 9), Interval<int>.OpenClosed(0, 3), Interval<int>.Open(1, 3), 
+                        Interval<int>.Open(2, 4), Interval<int>.Open(5, 7), Interval<int>.Empty()
+                    },
+                    new List<Interval<int>> {
+                        Interval<int>.Open(0, 4), Interval<int>.Closed(5, 9)
+                    }
+                },
+                new object[] {
+                    new List<Interval<int>> {
+                        Interval<int>.Closed(0, 1), Interval<int>.OpenClosed(3, 4), Interval<int>.Open(6, 7)
+                    },
+                    new List<Interval<int>> {
+                        Interval<int>.Closed(0, 1), Interval<int>.OpenClosed(3, 4), Interval<int>.Open(6, 7)
+                    }
+                },
+                new object[] {
+                    new List<Interval<int>> {
+                        Interval<int>.Closed(0, 1),
+                        Interval<int>.FromBounds(Bound<int>.NegativeInfinite(), Bound<int>.PositiveInfinite()), Interval<int>.Open(6, 7)
+                    },
+                    new List<Interval<int>> {
+                        Interval<int>.FromBounds(Bound<int>.NegativeInfinite(), Bound<int>.PositiveInfinite())
+                    }
+                },
+            };
+    }
+}
---
+++ CarloPantaleo.ComparableIntervals/Intervals.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarloPantaleo.ComparableIntervals {
+    /// <summary>
+    /// Utility class which provides some static methods to handle collections of intervals.
+    /// </summary>
+    public static class Intervals {
+        /// <summary>
+        /// Creates a collection of intervals without overlapping intervals by performing a union on the overlapping
+        /// intervals.
+        /// </summary>
+        /// <typeparam name="T">The <see cref="IComparable"/> type of the interval.</typeparam>
+        /// <returns>The resulting simplified collection.</returns>
+        public static List<Interval<T>> Simplify<T>(ICollection<Interval<T>> intervals) where T : IComparable {
+            var cleanIntervals = RemoveEmptyIntervals(intervals);
+            var resultingIntervals = PerformSimplify(cleanIntervals);
+            SortByLowerBound(resultingIntervals);
+
+            return resultingIntervals;
+        }
+
+        private static List<Interval<T>> PerformSimplify<T>(ICollection<Interval<T>> intervals) where T : IComparable {
+            var resultingIntervals = new List<Interval<T>>();
+            var ignoredIntervals = new List<Interval<T>>(); // Intervals already unified with others.
+
+            foreach (var inspectedInterval in intervals) {
+                var resultingInterval = inspectedInterval;
+                var intervalsToProcess = intervals
+                    .SkipWhile(i =>
+                        !ReferenceEquals(i, inspectedInterval)) // Skip intervals previous to the inspected one.
+                    .Skip(1) // Skip one more interval (which is exactly the inspected one).
+                    .Where(i => !ignoredIntervals.Contains(i)); // Exclude already ignored intervals.
+                foreach (var interval in intervalsToProcess) {
+                    if (!resultingInterval.Intersection(interval).IsEmpty()) {
+                        resultingInterval = resultingInterval.Union(interval);
+                        ignoredIntervals.Add(interval);
+                    }
+                }
+                
+                /*
+                 * Here checking if ignoredIntervals contains resultingInterval is not enough. We have to check if the
+                 * reference of resultingInterval is contained in ignoredIntervals, because resultingInterval may be a
+                 * union equal (by value) to a interval contained in ignoredInterval, but in that case it's a valid
+                 * interval to be added to the result (consider the case [2, 3], [1, 4] => the resulting interval has
+                 * to be [1, 4]).
+                 */
+                if (!ignoredIntervals.Any(ii => ReferenceEquals(ii, resultingInterval))) {
+                    resultingIntervals.Add(resultingInterval);
+                }
+            }
+
+            return resultingIntervals;
+        }
+
+        private static ICollection<Interval<T>> RemoveEmptyIntervals<T>(IEnumerable<Interval<T>> intervals)
+            where T : IComparable => intervals.Where(i => !i.IsEmpty()).ToList();
+
+        private static void SortByLowerBound<T>(List<Interval<T>> intervals) where T : IComparable =>
+            intervals.Sort((i, j) => i.LowerBound < j.LowerBound ? -1 : 1);
+    }
+}
